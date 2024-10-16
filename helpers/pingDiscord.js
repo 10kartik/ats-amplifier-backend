44829108e5f8b3874a5531c2c56e2c2f2933c7df
10kartik <10kartikkapgate@gmail.com>
2024-10-16 09:45:27 +0530

    updated logging to discord via webhook

--- helpers/pingDiscord.js
+++ helpers/pingDiscord.js
@@ -1,59 +1,31 @@
-// Import the Discord.js library
-const { Client, GatewayIntentBits } = require("discord.js");
+const axios = require("axios");
 
-// Create a new Discord client
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-});
-
-// Your bot token
-const token = process.env.DISCORD_BOT_TOKEN;
-
-// The ID of the channel you want to send a message to
-const channelId = process.env.DISCORD_ATS_CHANNEL_ID;
-
-// Log in to the bot
-client.login(token);
-
-function waitForClientReady() {
-  return new Promise((resolve) => {
-    if (client.isReady()) {
-      resolve();
-    } else {
-      client.once("ready", resolve);
-    }
-  });
-}
+// Your webhook URL
+const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
 
 async function sendMessageToDiscord(url, text, ClientInfo) {
-  await waitForClientReady();
+  const message = {
+    content:
+      "**====START================================**" +
+      "\n" +
+      `**Time:** ${new Date().toLocaleString("en-US", {
+        timeZone: "Asia/Kolkata",
+      })}` +
+      "\n" +
+      `**PDF:** ${url}` +
+      "\n" +
+      `**Keywords:** ${text}` +
+      "\n" +
+      `**Client Info:** \n ||${JSON.stringify(ClientInfo)} ||` +
+      "\n" +
+      "**====END====================================**",
+  };
 
-  const channel = client.channels.cache.get(channelId);
-
-  const message =
-    "**====START================================**" +
-    "\n" +
-    `**Time:** ${new Date().toLocaleString("en-US", {
-      timeZone: "Asia/Kolkata",
-    })}` +
-    "\n" +
-    `**PDF:** ${url}` +
-    "\n" +
-    `**Keywords:** ${text}` +
-    "\n" +
-    `**Client Info:** \n ||${JSON.stringify(ClientInfo)} ||` +
-    "\n" +
-    "**====END====================================**";
-
-  if (channel) {
-    channel.send(message);
+  try {
+    await axios.post(webhookUrl, message);
     console.log("Message sent to Discord successfully");
-  } else {
-    console.log("Channel not found");
+  } catch (error) {
+    console.error("Error sending message to Discord:", error.message);
   }
 }
 
